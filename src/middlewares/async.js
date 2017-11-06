5d07cfdf5801cacc6dce60d12bb421b73a59d71f
Nina Louw <ninalouw@mac.com>
2017-11-05 17:59:54 -0800

    complete custom async middleware to handle promises, reformat users reducer and user_list to take newly returned data

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,5 +1,18 @@
 export default function({ dispatch }){
     return next => action => {
-        next(action);
+        //if action does not have payload, or
+        //payload does not have a .then property
+        //we dont care about it, send it on
+        if(!action.payload || !action.payload.then){
+            return next(action);
+        }
+        //make sure the action's promise resolves
+        action.payload
+        .then(function(response){
+            //create a new action with the old type, but
+            //replace the promise with the response data
+            const newAction = {...action, payload: response }
+            dispatch(newAction);
+        });
     };
 }
